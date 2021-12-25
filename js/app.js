35a60af5ded4017b3406f3610a562707634c20ef
sajithnsilvame <usnc99@gmail.com>
2021-12-25 20:51:50 +0530

    Navigation bar with responsiness

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $(window).on('scroll',function(){
+        var scroll = $(window).scrollTop();
+        if(scroll>= 50){
+            $(".sticky").addClass("stickyAdd");
+        }
+        else{
+            $(".sticky").removeClass("stickyAdd");
+        }
+    })
+          
+})
+
+
+
+
+
+
+
+
